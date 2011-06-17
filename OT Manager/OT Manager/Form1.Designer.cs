951ced2a2b3bb6a243c93b986142f2e9979052de
unknown <carmen  bermudez@.(none)>
2011-06-17 01:31:20 -0430

    segundo commit

---
+++ OT Manager/OT Manager/Form1.Designer.cs
@@ -0,0 +1,69 @@
+﻿namespace OT_Manager
+{
+    partial class OTManager
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ScriptEditor = new System.Windows.Forms.RichTextBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // ScriptEditor
+            // 
+            this.ScriptEditor.Location = new System.Drawing.Point(12, 12);
+            this.ScriptEditor.Name = "ScriptEditor";
+            this.ScriptEditor.Size = new System.Drawing.Size(635, 426);
+            this.ScriptEditor.TabIndex = 0;
+            this.ScriptEditor.Text = "";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // OTManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(659, 450);
+            this.Controls.Add(this.ScriptEditor);
+            this.Name = "OTManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OT Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox ScriptEditor;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ OT Manager/OT Manager/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace OT_Manager
+{
+   
+
+
+    public partial class OTManager : Form
+    {
+        string Palabras_Reservadas = "";
+        public Regex keyWords = new Regex("abstract|");
+        public OTManager()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int selPos = ScriptEditor.SelectionStart;
+
+            foreach (Match keyWordMatch in keyWords.Matches(ScriptEditor.Text))
+
+            {
+                ScriptEditor.Select(keyWordMatch.Index, keyWordMatch.Length);
+
+                ScriptEditor.SelectionColor = Color.Blue;
+
+                ScriptEditor.SelectionStart = selPos;
+
+                ScriptEditor.SelectionColor = Color.Black;
+            }
+
+        }
+
+    }
+}
+
