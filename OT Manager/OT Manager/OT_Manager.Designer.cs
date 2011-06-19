62ce1a6e5b6094043ea8fcf2e0bc578757e1d8f3
leyendario <aljba3@hotmail.com>
2011-06-19 03:45:02 -0430

    agregado el lua

---
+++ OT Manager/OT Manager/OT_Manager.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace OT_Manager
+{
+    partial class OTManager
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben eliminar; false en caso contrario, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fastColoredTextBox1 = new FastColoredTextBoxNS.FastColoredTextBox();
+            this.SuspendLayout();
+            // 
+            // fastColoredTextBox1
+            // 
+            this.fastColoredTextBox1.AutoScrollMinSize = new System.Drawing.Size(25, 15);
+            this.fastColoredTextBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.fastColoredTextBox1.Location = new System.Drawing.Point(12, 66);
+            this.fastColoredTextBox1.Name = "fastColoredTextBox1";
+            this.fastColoredTextBox1.Size = new System.Drawing.Size(585, 372);
+            this.fastColoredTextBox1.TabIndex = 0;
+            // 
+            // OTManager
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(611, 450);
+            this.Controls.Add(this.fastColoredTextBox1);
+            this.Name = "OTManager";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "OT Manager";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private FastColoredTextBoxNS.FastColoredTextBox fastColoredTextBox1;
+
+    }
+}
+
