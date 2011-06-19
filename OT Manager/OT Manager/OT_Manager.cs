a98c77f666ff46b3ee0146dbe189c615a707075f
leyendario <aljba3@hotmail.com>
2011-06-19 03:45:02 -0430

    rewrite

---
+++ OT Manager/OT Manager/OT_Manager.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+
+namespace OT_Manager
+{
+    public partial class OTManager : Form
+    {
+        public OTManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
+
